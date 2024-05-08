39ddc71dece9579e6787e71e631a8f8d103fd683
JiShu Wang <cswangjishu@hotmail.com>
2024-05-08 13:42:50 +0800

    Update PSV Construction.py

--- PSV Construction/PSV Construction.py
+++ PSV Construction/PSV Construction.py
@@ -1,6 +1,5 @@
 import copy
 import json
-
 import cv2
 
 Images_Oringinal = {}
@@ -15,21 +14,12 @@ psviou, psvcount, deiou, decount = 0, 0, 0, 0
 
 
 def CalculateIoU(box1, box2):
-    # 获取矩形的四个坐标
     x1, y1, w1, h1 = box1
     x2, y2, w2, h2 = box2
-
-    # 计算两个矩形交集的左上角和右下角坐标
     left_top = (max(x1, x2), max(y1, y2))
     right_bottom = (min(x1 + w1, x2 + w2), min(y1 + h1, y2 + h2))
-
-    # 计算交集面积
     intersection_area = max(0, right_bottom[0] - left_top[0]) * max(0, right_bottom[1] - left_top[1])
-
-    # 计算并集面积
     union_area = w1 * h1 + w2 * h2 - intersection_area
-
-    # 计算IOU
     iou = intersection_area / union_area
     return iou
 
@@ -116,10 +106,6 @@ def ShowAnnotationImages(imageid):
     # cv2.imshow("Constructed" + str(imageid) + ".jpg", cv2.resize(image_constructed, None, fx=1, fy=1))
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    # cv2.imwrite("Result/Ground Truth/" + str(imageid) + "_GroundTruth.png", image_oringinal)
-    # cv2.imwrite("Result/Model Detection/" + str(model) + str(imageid) + "_Detection.png", image_detection)
-    # cv2.imwrite("Result/PSV Constructed/" + str(model) + str(imageid) + "_Construction.png", image_constructed)
-    # cv2.waitKey(0)
 
 
 def CalculateEffect(imageid):
@@ -164,7 +150,7 @@ def CalculateEffect(imageid):
 
 def PSVConstruct(model):
     global Images_Oringinal, Annotations_Constructed
-    for content in Images_Oringinal:  # 遍历所有的标注文件
+    for content in Images_Oringinal:
         imageid = content["id"]
         UnifyPosition(imageid, 0.9)
         UnifySpacing(imageid, 20, 0)
@@ -174,26 +160,26 @@ def PSVConstruct(model):
     print(deiou / decount, psviou / psvcount)
 
 
-def UnifyPosition(imageid, alpha):  # 2.统一位置
+def UnifyPosition(imageid, alpha):  # 1. Position Alignment
     global Annotations_Flitered
     for index, content in enumerate(Annotations_Flitered[imageid]):
         current = content
-        for reindex, recontent in enumerate(Annotations_Flitered[imageid][:index]):  # 与已有的停车位进行比较，根据重合度判定是否属于同一排或一列
+        for reindex, recontent in enumerate(Annotations_Flitered[imageid][:index]):  
             if index != reindex:
                 compare = recontent
                 overlapx, overlapy = 0, 0
                 if compare[0] <= current[0] <= compare[0] + compare[2] or current[0] <= compare[0] <= current[0] + \
-                        current[2]:  # X存在重合
+                        current[2]:  # The X-axis is existing overlap
                     overlapx = [current[0], current[0] + current[2], compare[0], compare[0] + compare[2]]
-                    overlapx.sort()  # 使用排序，则中间两个值的部分即为重合部分
-                    overlapx = overlapx[2] - overlapx[1]
+                    overlapx.sort()  
+                    overlapx = overlapx[2] - overlapx[1] # The overlap part
                 if compare[1] <= current[1] <= compare[1] + compare[3] or current[1] <= compare[1] <= current[1] + \
-                        current[3]:  # Y存在重合
+                        current[3]:  # The Y-axis is existing overlap
                     overlapy = [current[1], current[1] + current[3], compare[1], compare[1] + compare[3]]
                     overlapy.sort()
-                    overlapy = overlapy[2] - overlapy[1]
+                    overlapy = overlapy[2] - overlapy[1] # The overlap part
                 maxbox = max(current[2], compare[2])
-                if overlapx >= alpha * maxbox:  # 重合占比很高，可视作位于一列
+                if overlapx >= alpha * maxbox:  # The percent of overlap is high enough, and i and j can be thinked that is a same column
                     yspacing = [current[1], current[1] + current[3], compare[1], compare[1] + compare[3]]
                     yspacing.sort()
                     yspacing = yspacing[2] - yspacing[1]
@@ -201,54 +187,54 @@ def UnifyPosition(imageid, alpha):  # 2.统一位置
                         Annotations_Flitered[imageid][index][0] = recontent[0]
                         Annotations_Flitered[imageid][index][2] = recontent[2]
                 maxbox = max(current[3], compare[3])
-                if overlapy >= alpha * maxbox:  # 重合占比很高，可视作位于一排
+                if overlapy >= alpha * maxbox:  # The percent of overlap is high enough, and i and j can be thinked that is a same row
                     xspacing = [current[0], current[0] + current[2], compare[0], compare[0] + compare[2]]
                     xspacing.sort()
                     xspacing = xspacing[2] - xspacing[1]
                     if xspacing <= 50:
                         Annotations_Flitered[imageid][index][1] = recontent[1]
                         Annotations_Flitered[imageid][index][3] = recontent[3]
-        Annotations_Flitered[imageid].sort(key=lambda x: (x[1], x[0]))
+        Annotations_Flitered[imageid].sort(key=lambda x: (x[1], x[0])) # Ensure the loop order
 
 
-def UnifySpacing(imageid, beta, epsilon):  # 2.去除重叠(统一间距)
+def UnifySpacing(imageid, beta, epsilon):  # 2. Overlap Removal (Spacing Uniform)
     global Annotations_Flitered
     Xspacing, Yspacing = epsilon, epsilon
     for index, content in enumerate(Annotations_Flitered[imageid]):
         current = content
         Xdistance, Ydistance = beta, beta
-        for reindex, recontent in enumerate(Annotations_Flitered[imageid][:index]):  # 与已有的停车位进行比较，根据坐标判定是否重叠
+        for reindex, recontent in enumerate(Annotations_Flitered[imageid][:index]):  
             Xoindex, Yoindex, Xsindex, Ysindex = None, None, None, None
             compare = recontent
             overlapx, overlapy, spacingx, spacingy = 0, 0, 0, 0
-            if index != reindex and current[1] == compare[1]:  # 左右
+            if index != reindex and current[1] == compare[1]:  
                 overlapx = (compare[0] + compare[2]) - current[0]
                 spacingx = current[0] - (compare[0] + compare[2])
-                if compare[0] < current[0]:  # 当前车位位于所比较车位的右边
-                    if 0 < spacingx <= Xdistance:  # 存在间距
+                if compare[0] < current[0]:  
+                    if 0 < spacingx <= Xdistance:  
                         Xdistance = overlapx
                         Xsindex = reindex
-                    if overlapx > 0:  # 存在重叠
+                    if overlapx > 0:  
                         Xoindex = reindex
-            if index != reindex and current[0] == compare[0]:  # 上下
+            if index != reindex and current[0] == compare[0]:  
                 overlapy = (compare[1] + compare[3]) - current[1]
                 spacingy = current[1] - (compare[1] + compare[3])
-                if compare[1] < current[1]:  # 当前车位位于所比较车位的下方
+                if compare[1] < current[1]:  
                     if 0 < spacingy <= Ydistance:
                         Ydistance = spacingy
                         Ysindex = reindex
                     if overlapy > 0:
                         Yoindex = reindex
-            if Xsindex is not None and compare[0] + compare[2] < current[0]:  # X间距
+            if Xsindex is not None and compare[0] + compare[2] < current[0]:  # X-axis spacing
                 Annotations_Flitered[imageid][Xsindex][2] += spacingx / 2
                 Annotations_Flitered[imageid][index][0] -= spacingx / 2
-            if Xoindex is not None and compare[0] + compare[2] > current[0]:  # X重叠
+            if Xoindex is not None and compare[0] + compare[2] > current[0]:  # X-axis overlap
                 Annotations_Flitered[imageid][Xoindex][2] -= overlapx / 2
                 Annotations_Flitered[imageid][index][0] += overlapx / 2
-            if Ysindex is not None and current[1] > compare[1] + compare[3]:  # Y间距
+            if Ysindex is not None and current[1] > compare[1] + compare[3]:  # Y-axis spacing
                 Annotations_Flitered[imageid][Ysindex][3] += spacingy / 2
                 Annotations_Flitered[imageid][index][1] -= spacingy / 2
-            if Yoindex is not None and current[1] < compare[1] + compare[3]:  # Y重叠
+            if Yoindex is not None and current[1] < compare[1] + compare[3]:  # Y-axis overlap
                 Annotations_Flitered[imageid][Yoindex][3] -= overlapy / 2
                 Annotations_Flitered[imageid][index][1] += overlapy / 2
         UnifyPosition(imageid, 0.8)
